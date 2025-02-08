07556d4cfd11aeadabf00787a0debc58138d8450
Arnab Chakraborty <iam-arnab@outlook.com>
2025-02-09 00:28:19 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- experimental: {
-   reactCompiler: true
- }
+    experimental: {
+        reactCompiler: true,
+    },
 };
 
 export default nextConfig;
