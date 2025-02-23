58aea51cf64922bc47022c167c176bdedf94fad8
Arnab Chakraborty <iam-arnab@outlook.com>
2025-02-23 12:44:47 +0530

    Add Circle support to InfiniteCanvas and update state management

--- store/useCanvasStore.ts
+++ store/useCanvasStore.ts
@@ -4,27 +4,33 @@ interface CanvasState {
     lines: DrawLine[];
     textElements: TextElement[];
     rectangles: Rectangle[];
+    circles: Circle[];
     history: {
         lines: DrawLine[];
         textElements: TextElement[];
         rectangles: Rectangle[];
+        circles: Circle[];
     }[];
     currentStep: number;
     setLines: (lines: DrawLine[]) => void;
-    addToHistory: (lines: DrawLine[] | TextElement[] | Rectangle[]) => void;
+    addToHistory: (
+        lines: DrawLine[] | TextElement[] | Rectangle[] | Circle[],
+    ) => void;
     undo: () => void;
     redo: () => void;
     canUndo: () => boolean;
     canRedo: () => boolean;
     setTextElements: (textElements: TextElement[]) => void;
     setRectangles: (rectangles: Rectangle[]) => void;
+    setCircles: (circles: Circle[]) => void;
 }
 
 export const useCanvasStore = create<CanvasState>((set, get) => ({
     lines: [],
     textElements: [],
     rectangles: [],
-    history: [{ lines: [], textElements: [], rectangles: [] }],
+    circles: [],
+    history: [{ lines: [], textElements: [], rectangles: [], circles: [] }],
     currentStep: 0,
 
     setTextElements: (textElements) => {
@@ -39,21 +45,36 @@ export const useCanvasStore = create<CanvasState>((set, get) => ({
         set({ rectangles });
     },
 
-    addToHistory: (elements: DrawLine[] | TextElement[] | Rectangle[]) => {
-        const { currentStep, history, lines, textElements, rectangles } = get();
+    setCircles: (circles) => {
+        set({ circles });
+    },
+
+    addToHistory: (
+        elements: DrawLine[] | TextElement[] | Rectangle[] | Circle[],
+    ) => {
+        const {
+            currentStep,
+            history,
+            lines,
+            textElements,
+            rectangles,
+            circles,
+        } = get();
         const newHistory = history.slice(0, currentStep + 1);
         const isDrawLines = elements[0] && 'points' in elements[0];
         const isRectangles = elements[0] && 'width' in elements[0];
+        const isCircles = elements[0] && 'radius' in elements[0];
 
         newHistory.push({
             lines: isDrawLines ? [...(elements as DrawLine[])] : [...lines],
             textElements:
-                !isDrawLines && !isRectangles
+                !isDrawLines && !isRectangles && !isCircles
                     ? [...(elements as TextElement[])]
                     : [...textElements],
             rectangles: isRectangles
                 ? [...(elements as Rectangle[])]
                 : [...rectangles],
+            circles: isCircles ? [...(elements as Circle[])] : [...circles],
         });
 
         set({
@@ -61,10 +82,11 @@ export const useCanvasStore = create<CanvasState>((set, get) => ({
             currentStep: currentStep + 1,
             lines: isDrawLines ? (elements as DrawLine[]) : lines,
             textElements:
-                !isDrawLines && !isRectangles
+                !isDrawLines && !isRectangles && !isCircles
                     ? (elements as TextElement[])
                     : textElements,
             rectangles: isRectangles ? (elements as Rectangle[]) : rectangles,
+            circles: isCircles ? (elements as Circle[]) : circles,
         });
     },
 
@@ -77,6 +99,7 @@ export const useCanvasStore = create<CanvasState>((set, get) => ({
                 lines: [...history[newStep].lines],
                 textElements: [...history[newStep].textElements],
                 rectangles: [...history[newStep].rectangles],
+                circles: [...history[newStep].circles],
             });
         }
     },
@@ -90,6 +113,7 @@ export const useCanvasStore = create<CanvasState>((set, get) => ({
                 lines: [...history[newStep].lines],
                 textElements: [...history[newStep].textElements],
                 rectangles: [...history[newStep].rectangles],
+                circles: [...history[newStep].circles],
             });
         }
     },
