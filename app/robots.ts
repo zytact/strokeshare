974e342065b35447962d80c8206d6eb897678e10
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-05 14:32:07 +0530

    add sitemap url to robot

--- app/robots.ts
+++ app/robots.ts
@@ -6,5 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
+        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
     };
 }
