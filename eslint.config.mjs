496507504821e0a229f7f452c269e21550886b9b
Arnab Chakraborty <that.zytact@gmail.com>
2025-12-07 20:49:25 +0530

    use eslint cli

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,19 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-    ...compat.extends('next/core-web-vitals', 'next/typescript'),
-    eslintPluginPrettierRecommended,
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  }
 ];
 
 export default eslintConfig;
